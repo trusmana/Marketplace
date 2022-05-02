b53e5eacb55f55200eda73c941d71489ab45495b
trusmana <tedyrusmanawinangoen@gmail.com>
2022-05-02 21:36:48 +0700

    Your branch is up-to-date with 'origin/01/origin/master'. Changes to be committed:  Untracked files:

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from apps.accounts.models import AccountsUser as user
 
-# Create your views here.
+@login_required
+def home_member(request):
+    user = request.user
+    return render(request,'member/home-member.html',{'user':user})
+
+
+def error_404(request,exception=None):
+    context = {}
+    return render(request,'core/error_500.html',context)
+
+def error_500(request):
+    context = {}
+    return render(request,'core/error_500.html', context)
--- apptrusmana/settings.py
+++ apptrusmana/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6w9ss8+-%w^gd+o3yi(r1qwr0dhy@ul3ec)s6^f31hv16=!6#y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','*']
 
 
 # Application definition
@@ -124,14 +124,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'home-member'
 LOGOUT_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR ,"media")
 AUTH_USER_MODEL = 'accounts.AccountsUser'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
